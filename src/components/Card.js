d75e08fd215c72c4e3b8f1b9ca5bfc953852a04c
taichikoumoto <sbskomoto@gmail.com>
2018-01-17 11:10:02 +0900

    完成

--- src/components/Card.js
+++ src/components/Card.js
@@ -14,7 +14,7 @@ const styles = {
     borderWidth: 1,
     borderRadius: 2,
     borderColor: '#ddd',
-    bordorBottomWidth: 0,
+    borderBottomWidth: 0,
     shadowOffset: { width: 0, height: 2 },
     shadowOpacity: 0.1,
     shadowRadius: 2,
